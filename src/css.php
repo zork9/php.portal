6bd76c93b9d376d770188be2196ffa8489676923
Johan Ceupens <johan@sigtrans.org>
2013-12-15 11:57:41 +0100

    css work

---
+++ src/css.php
@@ -0,0 +1,38 @@
+<?php
+
+/* css page piece */
+
+/*
+ Copyright (C) Johan Ceuppens 2013
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 2 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+?>
+
+<?php
+include('util.php');
+include('cssutil.php');
+include('cssjs.php');
+//FIX use JS widgets for changing the look and feel
+
+printHeader();
+printBodyInit();
+
+echo '<form id="start" method="post" action="cssfirstpass.php">';
+echo '<input type="text" name="csswritetext">';
+echo '<input type="submit" name="csswritetextsubmit">';
+echo '</form>';
+printBodyHTMLClose();
+
+?>
